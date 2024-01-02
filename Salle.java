23efdd2802ced7c83331ab681e53ee3ae8f5b7da
zeynelUbiqube <zal@ubiqube.com>
2024-01-02 21:59:45 +0100

    bis

--- Salle.java
+++ Salle.java
@@ -1,19 +1,80 @@
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Salle {
     List<GroupeDeRangees> groupes;
+    int P, K, Q; 
 
     public Salle(){
         groupes = new ArrayList<GroupeDeRangees>();
+        P = 0;
+        K = 0;
+        Q = 0;
     }
 
     public Salle(List<GroupeDeRangees> groupes){
         this.groupes = new ArrayList<GroupeDeRangees>(groupes);
+        P = 0;
+        K = 0;
+        Q = 0;
     }
+
+    public Salle(List<GroupeDeRangees> groupes, int P, int K, int Q){
+        this.groupes = new ArrayList<GroupeDeRangees>(groupes);
+        this.P = P;
+        this.K = K;
+        this.Q = Q;
+    }
+
 
     public void addGroupe(GroupeDeRangees groupe){
         groupes.add(new GroupeDeRangees(groupe.rangees));
+    }
+
+    public void readSalleData(String fileName) {
+        try {
+            File fichier = new File(fileName);
+            Scanner sc = new Scanner(fichier);
+            
+            Salle salle = new Salle();
+
+            int nbGroupe = sc.nextInt();
+
+            List<Integer> nbRangeGroup = new ArrayList<Integer>();
+
+            for (int i = 0; i < nbGroupe; i++) {
+                nbRangeGroup.add(sc.nextInt());
+            }
+            
+            for (Integer x : nbRangeGroup) {
+                GroupeDeRangees gr = new GroupeDeRangees();
+                for (int i = 0; i < x; i++) {
+                    int capacitee = sc.nextInt();
+                    int distanceDeLaScene = sc.nextInt();
+                    Rangees r = new Rangees(capacitee, distanceDeLaScene);
+                    gr.addRangees(r);
+                }
+                this.addGroupe(gr);
+            }
+            sc.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void readConstraintData(String fileName){
+        try {
+            File fichier = new File(fileName);
+            Scanner sc = new Scanner(fichier);
+            P = sc.nextInt();
+            K = sc.nextInt();
+            Q = sc.nextInt();
+            sc.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public String ToString(){
@@ -26,6 +87,9 @@ public class Salle {
             }
             res = res + "\n";
         }
+        res = res + "P : " + P + "\n";
+        res = res + "K : " + K + "\n";
+        res = res + "Q : " + Q + "\n";
         return res;
     }
 }
--- test.java
+++ test.java
@@ -1,6 +1,9 @@
 public class test {
     public static void main(String[] args) {
-        Salle s = new getSalleData().readSalleData("DATA_A/Salle01/Salle01.txt");
+        // Salle s = new getSalleData().readSalleData("DATA_A/Salle01/Salle01.txt");
+        Salle s = new Salle();
+        s.readSalleData("DATA_A/Salle01/Salle01.txt");
+        s.readConstraintData("DATA_A/Salle01/Contraintes01..txt");
         System.out.println(s.ToString());
     }
 }
