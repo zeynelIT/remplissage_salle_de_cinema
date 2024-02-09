efb709c2cf13d347704a8191bd22a5861ec4d8a0
Alae Benmoussa <Alae.Benmoussa@grenoble-inp.org>
2024-02-09 16:04:07 +0100

    checker qui vérifie P,k et Q

--- test.java
+++ test.java
@@ -4,10 +4,7 @@ public class test {
         s.readSalleData("DATA_A/Salle06/Salle06.txt");
         s.readConstraintData("DATA_A/Salle06/Contraintes01..txt");
         s.readReservationData("DATA_A/Salle06/Reservations01.txt");
-        System.out.println(s.ToString());
         Remplissage remplissage = new Remplissage(s);
-        System.out.println(remplissage.data.size());
-        System.out.println(remplissage.salle.ToString());
         System.out.println(remplissage.ToString());
     }
 }
